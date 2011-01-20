914a07c64f0e315a1efc02e857a5b77db6005a96
nicola.montecchio <nicola.montecchio@gmail.com>
2011-01-20 13:56:19 +0100

    added conversion scripts for chroma matrix formats

---
+++ scripts/falcon2csv.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+
+# Convert a chroma matrix from falcon format
+# to simpler csv format. Operates on stdin and stdout.
+# Does not check for errors.
+
+import sys
+import csv
+
+__author__="nicolamontecchio"
+__date__ ="$Jan 20, 2011 1:22:02 PM$"
+
+if __name__ == "__main__" :
+	reader = csv.reader(sys.stdin, delimiter=';')
+	lineno = 0
+	for line in reader :
+		if lineno >= 2 :
+			print ','.join(line[:-1])
+		lineno += 1
+	
+
+
+	
+	
+		
