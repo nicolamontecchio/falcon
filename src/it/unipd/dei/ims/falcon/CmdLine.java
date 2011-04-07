4146c3169ad4afc03d01f3f3470eedd331784ea0
Nicola Montecchio <nicola.montecchio@gmail.com>
2011-04-07 13:15:06 +0200

    added evaluation script, almost finished changing main

--- src/it/unipd/dei/ims/falcon/CmdLine.java
+++ src/it/unipd/dei/ims/falcon/CmdLine.java
@@ -15,12 +15,11 @@ package it.unipd.dei.ims.falcon;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import java.io.FileNotFoundException;
 import static java.util.Arrays.asList;
 
 import it.unipd.dei.ims.falcon.analysis.chromafeatures.ChromaMatrixUtils;
 import it.unipd.dei.ims.falcon.analysis.transposition.TranspositionEstimator;
-import it.unipd.dei.ims.falcon.analysis.transposition.TranspositionFileEntry;
-import it.unipd.dei.ims.falcon.analysis.transposition.TranspositionFileParser;
 import it.unipd.dei.ims.falcon.indexing.Indexing;
 import it.unipd.dei.ims.falcon.indexing.IndexingException;
 import it.unipd.dei.ims.falcon.ranking.DocScorePair;
@@ -29,11 +28,16 @@ import it.unipd.dei.ims.falcon.ranking.QueryParser;
 import it.unipd.dei.ims.falcon.ranking.QueryParsingException;
 import it.unipd.dei.ims.falcon.ranking.QueryPruningStrategy;
 import it.unipd.dei.ims.falcon.ranking.StaticQueryPruningStrategy;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -56,14 +60,13 @@ public class CmdLine {
 			+ "ncf:0.338766*[0.001601,0.995038];nmf:0.331577*[0.002352,0.997884];";
 
 	// conversion step
-	// TODO add the 'notransposition' option
 	private static void conversion(OptionSet cmdline_options) throws IOException {
 		File inputdir = new File((String) cmdline_options.valueOf("datapath"));
 		File outBaseDir = new File((String) cmdline_options.valueOf("convertdir"));
 		int nranks = 3;
-		double minkurtosis = 3.;
-		TranspositionEstimator te = new TranspositionEstimator(3, null);
-		int ntransp = (Integer) cmdline_options.valueOf("ntransp");
+		double minkurtosis = 0.;
+		TranspositionEstimator te = cmdline_options.has("nokeyfind") ? null : new TranspositionEstimator(3, null);
+		int ntransp = cmdline_options.has("nokeyfind") ? 1 : (Integer) cmdline_options.valueOf("ntransp");
 		ChromaMatrixUtils.convertdir(inputdir, outBaseDir, nranks, minkurtosis, te, ntransp);
 	}
 
@@ -75,10 +78,22 @@ public class CmdLine {
 			int hashPerSegment = (Integer) cmdline_options.valueOf("hashperseg");
 			int segmentOverlap = (Integer) cmdline_options.valueOf("overlap");
 			Indexing.index(indexPath, dataPath, hashPerSegment, segmentOverlap);
-
 		} catch (IndexingException ex) {
 			Logger.getLogger(CmdLine.class.getName()).log(Level.SEVERE, null, ex);
 		}
+	}
+
+	// load query files from a txt file, one per line
+	private static List<String> readQueryFileList(String path) throws FileNotFoundException, IOException {
+		List<String> qfiles = new LinkedList<String>();
+		FileInputStream fstream = new FileInputStream(path);
+		DataInputStream in = new DataInputStream(fstream);
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		String strLine;
+		while ((strLine = br.readLine()) != null) {
+			qfiles.add(strLine.trim());
+		}
+		return qfiles;
 	}
 
 	// ranking step
@@ -88,9 +103,9 @@ public class CmdLine {
 		String queryDataPath = (String) cmdline_options.valueOf("querypath");
 		int hashPerSegment = (Integer) cmdline_options.valueOf("hashperseg");
 		int segmentOverlap = (Integer) cmdline_options.valueOf("overlap");
-		if (cmdline_options.has("lambda")) {
-			QueryMethods.setLambda((Float) cmdline_options.valueOf("lambda"));
-		}
+//		if (cmdline_options.has("lambda")) {
+//			QueryMethods.setLambda((Float) cmdline_options.valueOf("lambda"));
+//		}
 		// query pruning strategy (default value, can be overridden or set to null if option is not given)
 		String ss = cmdline_options.has("qps")
 				? (cmdline_options.valueOf("qps") != null
@@ -101,13 +116,19 @@ public class CmdLine {
 
 
 		// load file names in 0/ subdirectory
-		String[] filenames = (new File(queryDataPath, "0")).list();
-		int ntransp = cmdline_options.has("ntransp") ? (Integer) cmdline_options.valueOf("ntransp") : 3;
-
+		List<String> filenames;
+		if (cmdline_options.has("qfl")) {
+			filenames = readQueryFileList((String) cmdline_options.valueOf("qfl"));
+		} else {
+			String[] ff = (new File(queryDataPath, "0")).list();
+			filenames = new ArrayList<String>();
+			for (String f : ff) {
+				filenames.add(f);
+			}
+		}
+		int ntransp = cmdline_options.has("nk") ? 1 : cmdline_options.has("ntransp") ? (Integer) cmdline_options.valueOf("ntransp") : 3;
 		for (String f : filenames) {
 			// depending on just qp or also qf, do a whole directory or a single query
-			// TODO complete here
-			// TODO here also goes the 'notransp' stuff
 			List<File> queryFilesList = new LinkedList<File>();
 			for (int i = 0; i < ntransp; i++) {
 				File subdir = new File(queryDataPath, String.format("%d", i));
@@ -121,6 +142,7 @@ public class CmdLine {
 			Map<String, Double> songid2finalscore =
 					QueryMethods.performQuery(queryFilesList, new File(indexPath), hashPerSegment, segmentOverlap, hqps);
 			int r = 1;
+			System.out.println(String.format("QUERY - %s", f));
 			for (DocScorePair p : DocScorePair.docscore2scoredoc(songid2finalscore)) {
 				System.out.println(String.format("rank %3d: %10.6f - %s", r++, p.getScore(), p.getDoc()));
 			}
@@ -130,9 +152,6 @@ public class CmdLine {
 
 	public static void main(String[] args) throws IOException, QueryParsingException {
 
-		System.out.println(cmdline_notice);
-		// command line processing stuff
-		// -----------------------------------------------------------
 		OptionParser parser = new OptionParser() {
 
 			{
@@ -142,7 +161,7 @@ public class CmdLine {
 				acceptsAll(asList("r", "ranking"), "perform ranking");
 				acceptsAll(asList("ip", "indexpath")).withRequiredArg().ofType(String.class);
 				acceptsAll(asList("qp", "querypath"), "path for query files").withRequiredArg().ofType(String.class);
-				acceptsAll(asList("qf", "queryfile"), "single-file query").withRequiredArg().ofType(String.class);
+				acceptsAll(asList("qfl", "queryfilelist"), "list of individual queries to perform").withRequiredArg().ofType(String.class);
 				acceptsAll(asList("dp", "datapath")).withRequiredArg().ofType(String.class);
 				acceptsAll(asList("cql"), "chroma quantization levels").withRequiredArg().ofType(Integer.class);
 				acceptsAll(asList("cd", "convertdir"), "convert files in data path to hash " + "representation").withRequiredArg().ofType(String.class).describedAs("output base directory");
@@ -150,6 +169,7 @@ public class CmdLine {
 				acceptsAll(asList("overlap")).withRequiredArg().ofType(Integer.class);
 				acceptsAll(asList("lambda")).withRequiredArg().ofType(Float.class);
 				acceptsAll(asList("tf", "transpfile")).withRequiredArg().ofType(String.class);
+				acceptsAll(asList("nk", "nokeyfind"), "do not perform key finding");
 				acceptsAll(asList("nt", "ntransp"),
 						"number of transpositions to try when querying, default 3 (at most)").withRequiredArg().ofType(
 						Integer.class);
